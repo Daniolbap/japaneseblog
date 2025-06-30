88303776e13ef8689f6ea5749ad7384db32df2fd
daniolbap <chenso-dani@hotmail.com>
2025-06-30 17:59:44 -0600

    ahora si todo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
-app.use(express.static("public")); // to use images and styles in file named public and express look there for them
+app.use(express.static("public"));
