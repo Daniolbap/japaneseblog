8378e311f4e52d706d3992a919f22aa177cd72e4
daniolbap <chenso-dani@hotmail.com>
2025-07-10 20:25:40 -0600

    agregando paginas extras y estilizandolo

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/faqs", (req, res) => {
+  res.render("faqs.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/everything", (req, res) => {
+  res.render("everithing.ejs");
+});
 app.use(express.static("public"));
 
-console.log(new Date().getFullYear());
+console.log();
